d8e36b12867478e00c69f73e4395f52ceaf0eefc
Daphni <daphng@uw.edu>
2023-11-22 20:12:52 -0800

    added my changes to products page

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ProductCards from './ProductCards'; // Assuming the path is correct
+import ProductCards from './ProductCards';
 import cleanserImage from './img/cleanser1.png';
 import cleanserImage2 from './img/cleanser2.png';
 import cleanserImage3 from './img/cleanser3.png';
@@ -7,75 +7,83 @@ import cleanserImage4 from './img/cleanser4.png';
 import cleanserImage5 from './img/cleanser5.png';
 import cleanserImage6 from './img/cleanser6.png';
 
-const ProductPage = () => {
-  const [sortByPrice, setSortByPrice] = useState(false);
 
-  // Product data
-  const productsData = 
-    [
-        {
-          image: cleanserImage,
-          alt: 'Cleanser 1',
-          description:
-            'Cetaphil Face Wash, Daily Facial Cleanser for Sensitive, Combination to Oily Skin, NEW 16 oz, Fragrance Free, Gentle Foaming, Soap Free, Hypoallergenic',
-          price: '$10.99', // Set the price for the product
-        },
-        {
-          image: cleanserImage2,
-          alt: 'pic of cleanser',
-          description:
-            'CeraVe Hydrating Facial Cleanser | Moisturizing Non-Foaming Face Wash with Hyaluronic Acid, Ceramides and Glycerin | Fragrance Free Paraben Free | 16 Fluid Ounce',
-          price: '$12.49', // Set the price for the product
-        },
-        {
-          image: cleanserImage3,
-          alt: 'Cleanser 13',
-          description:
-            'Clean & Clear Morning Burst Facial Cleanser, Original,8 Ounce',
-          price: '$15.99', // Set the price for the product
-        },
-        {
-          image: cleanserImage4,
-          alt: 'pic of cleanser',
-          description:
-            'La Roche-Posay Toleriane Hydrating Gentle Face Cleanser, Daily Facial Cleanser with Niacinamide and Ceramides for Sensitive Skin, Moisturizing Face Wash for Normal to Dry Skin, Fragrance Free',
-          price: '$16.49', // Set the price for the product
-        },
-        {
-          image: cleanserImage5,
-          alt: 'pic of cleanser',
-          description:
-            'Youth To The People Facial Cleanser - Kale and Green Tea Cleanser - Gentle Face Wash, Makeup Remover + Pore Minimizer for All Skin Types - Vegan (8oz)',
-          price: '$9.49', // Set the price for the product
-        },
-        {
-          image: cleanserImage6,
-          alt: 'pic of cleanser',
-          description:
-            'TATCHA The Rice Wash | Soft Cream Facial Cleanser Washes Away Buildup Without Stripping Skin For A Soft, Luminous Complexion | 4 oz',
-          price: '$13.49', // Set the price for the product
-        },
-        
-      ];
-    
+const ProductPage = () => {
+ const [sortByPrice, setSortByPrice] = useState(false);
+
+
+ // Product data
+ const productsData =
+   [
+       {
+         image: cleanserImage,
+         alt: 'Cleanser 1',
+         description:
+           'Cetaphil Face Wash, Daily Facial Cleanser for Sensitive, Combination to Oily Skin, NEW 16 oz, Fragrance Free, Gentle Foaming, Soap Free, Hypoallergenic',
+         price: '$10.99',
+       },
+       {
+         image: cleanserImage2,
+         alt: 'pic of cleanser',
+         description:
+           'CeraVe Hydrating Facial Cleanser | Moisturizing Non-Foaming Face Wash with Hyaluronic Acid, Ceramides and Glycerin | Fragrance Free Paraben Free | 16 Fluid Ounce',
+         price: '$12.49',
+       },
+       {
+         image: cleanserImage3,
+         alt: 'Cleanser 13',
+         description:
+           'Clean & Clear Morning Burst Facial Cleanser, Original,8 Ounce, Gentle Foaming, Soap Free, Hypoallergenic, Fragrance Free Paraben Free ',
+         price: '$15.99',
+       },
+       {
+         image: cleanserImage4,
+         alt: 'pic of cleanser',
+         description:
+           'La Roche-Posay Toleriane Hydrating Gentle Face Cleanser, Daily Facial Cleanser with Niacinamide and Ceramides for Sensitive Skin, Moisturizing Face Wash for Normal to Dry Skin, Fragrance Free',
+         price: '$16.49',
+       },
+       {
+         image: cleanserImage5,
+         alt: 'pic of cleanser',
+         description:
+           'Youth To The People Facial Cleanser - Kale and Green Tea Cleanser - Gentle Face Wash, Makeup Remover + Pore Minimizer for All Skin Types - Vegan (8oz)',
+         price: '$9.49',
+       },
+       {
+         image: cleanserImage6,
+         alt: 'pic of cleanser',
+         description:
+           'TATCHA The Rice Wash | Soft Cream Facial Cleanser Washes Away Buildup Without Stripping Skin For A Soft, Luminous Complexion | 4 oz',
+         price: '$13.49',
+       },
+      
+     ];
   
 
-  return (
-    <div>
-      {/* Filter controls and other elements */}
-      {/* For instance, the checkbox for sorting by price */}
-      <input
-        type="checkbox"
-        id="price-checkbox"
-        onChange={() => setSortByPrice(!sortByPrice)}
-        checked={sortByPrice}
-      />
-      <label htmlFor="price-checkbox">Sort by Price</label>
 
-      {/* Pass the product data and sorting state to ProductCards */}
-      <ProductCards products={productsData} sortByPrice={sortByPrice} />
-    </div>
-  );
+ return (
+   <div>
+     <section className="filters">
+      <h2>Filters</h2>
+     <ul className="boxes">
+     <li>
+     <input
+       type="checkbox"
+       id="price-checkbox"
+       onChange={() => setSortByPrice(!sortByPrice)}
+       checked={sortByPrice}
+     />
+      <label htmlFor="price-checkbox">Sort by Price</label>
+     </li>
+     </ul>
+     </section>
+  
+   
+     <ProductCards products={productsData} sortByPrice={sortByPrice} />
+   </div>
+ );
 };
+
 
 export  {ProductPage};
--- src/components/product-routine.js
+++ src/components/product-routine.js
@@ -1,24 +1,5 @@
 import React, { useState } from 'react';
-import icon from './img/backdrop.jpg';
 
-export function NavBar() {
-    return (
-        <nav>
-            <div id="hamburger-menu">
-                <a href="#"><i class="fa fa-bars" aria-label="menu"></i></a></div>
-            <div id="menubar">
-                <ul>
-                    <li><a href="index.html">My Profile</a></li>
-                    <li><a href="product-routine.html">Skincare Routine</a></li>
-                    <li><a href="products.html"> Build Routine</a></li>
-                    <li><a href="quiz.html">Skin Quiz</a></li>
-                </ul>
-            </div>
-            <img src={icon} alt="Navigation Icon" />
-            <p>Skintellect</p>
-        </nav>
-    )
-}
 
 const ProductColumn = () => {
   const [productStates, setProductStates] = useState({
